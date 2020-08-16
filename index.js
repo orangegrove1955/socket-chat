31c0f1e61d41b4b78f9205ec00d04a58125071c8
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-08-16 12:35:51 +1000

    Restructuring of server and self message appends to allow styling of messages

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const messageInput = document.getElementById("message-input");
 
 window.onload = () => {
   const name = prompt("What is your name?");
-  appendMessage("You have joined the chat");
+  appendNotificationMessage("You have joined the chat");
   socket.emit("new-user", name);
 };
 
 // Display messages when incoming
 socket.on("chat-message", (data) => {
-  appendMessage(`${data.name}: ${data.message}`);
+  appendServerMessage(data.name, data.message);
 });
 
 /* Display when new users join the chat */
 socket.on("user-connected", (name) => {
-  appendMessage(`${name} has joined the chat`);
+  appendNotificationMessage(`${name} has joined the chat`);
 });
 
 /* Display when a user leaves the chat */
 socket.on("user-disconnected", (name) => {
-  appendMessage(`${name} has left the chat`);
+  appendNotificationMessage(`${name} has left the chat`);
 });
 
 // Add event listener to send message when message form is submitted
@@ -34,13 +34,40 @@ messageForm.addEventListener("submit", (e) => {
 const sendMessage = () => {
   const message = messageInput.value;
   socket.emit("send-message", message);
-  appendMessage(`You: ${message}`);
+  appendMessage(message);
   messageInput.value = "";
 };
 
 /** Append a new message to the chat */
 const appendMessage = (message) => {
   const messageElement = document.createElement("div");
+  messageElement.classList.add("self");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+};
+
+/** Append a new server message to the chat */
+const appendServerMessage = (name, message) => {
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("server");
+  // Add name
+  const nameElement = document.createElement("p");
+  nameElement.classList.add("name");
+  nameElement.innerText = name;
+  // Add message
+  const messageElement = document.createElement("p");
+  messageElement.classList.add("server-message");
+  messageElement.innerText = message;
+
+  containerElement.append(nameElement);
+  containerElement.append(messageElement);
+  messageContainer.append(containerElement);
+};
+
+/** Append a new server message to the chat */
+const appendNotificationMessage = (message) => {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("notification");
   messageElement.innerText = message;
   messageContainer.append(messageElement);
 };
