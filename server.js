9d56ad6b5955ee1c8f110957a6eac632b01dc51e
orangegrove_1955 <orangegrove_1955@hotmail.com>
2021-01-28 21:32:06 +1100

    Added ability to create multiple rooms using EJS rendering

--- server.js
+++ server.js
@@ -3,11 +3,45 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
 const users = {};
+const rooms = { Name: {} };
 
-app.use(express.static(__dirname));
+// Render the homepage
+app.get("/", (req, res) => {
+  res.render("index", { rooms: rooms });
+});
+
+app.post("/createRoom", (req, res) => {
+  if (rooms[req.body.roomName] != null) {
+    // return res.send({ status: 400, message: "Room already exists" });
+    // TODO - add redirect to home with error popup for room already exists
+    return res.redirect(req.body.roomName);
+  }
+  if (req.body.roomName) {
+    rooms[req.body.roomName] = { users: {} };
+    console.log(`Created new room ${req.body.roomName}`);
+    res.redirect(req.body.roomName);
+
+    // Send message that new room was created
+    return;
+  }
+  return res.send({ status: 400, message: "Failed to create room" });
+});
+
+// Render a room passed by URL param
+app.get("/:room", (req, res) => {
+  if (Object.keys(rooms).includes(req.params.room)) {
+    return res.render("room", { roomName: req.params.room });
+  }
+  return res.redirect("/");
+});
 
 io.on("connection", (socket) => {
   /** New user has joined the chat */
